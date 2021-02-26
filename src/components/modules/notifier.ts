bd857052dda120c96e4272a2509e7bb6f0d9f319
ChasLui <chaslui@outlook.com>
2021-02-27 00:19:01 +0800

    docs: 通知模块

--- src/components/modules/notifier.ts
+++ src/components/modules/notifier.ts
@@ -1,20 +1,20 @@
 import Module from '../__module';
 
 /**
- * Use external package module for notifications
+ * 使用外部软件包模块进行通知
  *
  * @see https://github.com/codex-team/js-notifier
  */
 import notifier, { ConfirmNotifierOptions, NotifierOptions, PromptNotifierOptions } from 'codex-notifier';
 
 /**
- * Notifier module
+ * 通知模块
  */
 export default class Notifier extends Module {
   /**
-   * Show web notification
+   * 显示网页通知
    *
-   * @param {NotifierOptions | ConfirmNotifierOptions | PromptNotifierOptions} options - notification options
+   * @param {NotifierOptions | ConfirmNotifierOptions | PromptNotifierOptions} options - 通知选项
    */
   public show(options: NotifierOptions | ConfirmNotifierOptions | PromptNotifierOptions): void {
     notifier.show(options);
