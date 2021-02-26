47787e17eea6a0ceb59eb8282beced828bb9c872
ChasLui <chaslui@outlook.com>
2021-02-27 00:17:57 +0800

    docs: 块选择器

--- src/components/modules/blockSelection.ts
+++ src/components/modules/blockSelection.ts
@@ -1,6 +1,6 @@
 /**
  * @class BlockSelection
- * @classdesc Manages Block selection with shortcut CMD+A
+ * @classdesc 使用快捷键 CMD + A 管理块选择
  *
  * @module BlockSelection
  * @version 1.0.0
@@ -18,16 +18,15 @@ import { SanitizerConfig } from '../../../types/configs';
  */
 export default class BlockSelection extends Module {
   /**
-   * Sometimes .anyBlockSelected can be called frequently,
-   * for example at ui@selectionChange (to clear native browser selection in CBS)
-   * We use cache to prevent multiple iterations through all the blocks
+   * 有时 .anyBlockSelected 可以被频繁调用，例如在 ui@selectionChange 上（以清除CBS中的本机浏览器选择）
+   * 我们使用缓存来防止遍历所有块
    *
    * @private
    */
   private anyBlockSelectedCache: boolean | null = null;
 
   /**
-   * Sanitizer Config
+   * 消毒配置
    *
    * @returns {SanitizerConfig}
    */
@@ -59,7 +58,7 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Flag that identifies all Blocks selection
+   * 标识所有块选择的标志
    *
    * @returns {boolean}
    */
@@ -70,9 +69,9 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Set selected all blocks
+   * 设置选中的所有块
    *
-   * @param {boolean} state - state to set
+   * @param {boolean} state - 设置状态
    */
   public set allBlocksSelected(state: boolean) {
     const { BlockManager } = this.Editor;
@@ -85,7 +84,7 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Flag that identifies any Block selection
+   * 标识任何块选择的标志
    *
    * @returns {boolean}
    */
@@ -100,7 +99,7 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Return selected Blocks array
+   * 返回选中的数组
    *
    * @returns {Block[]}
    */
@@ -109,40 +108,40 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Flag used to define block selection
-   * First CMD+A defines it as true and then second CMD+A selects all Blocks
+   * 用于定义块选​​择的标志
+   * 第一个 CMD + A 将其定义为 true，然后第二个 CMD + A 选择所有块
    *
    * @type {boolean}
    */
   private needToSelectAll = false;
 
   /**
-   * Flag used to define native input selection
-   * In this case we allow double CMD+A to select Block
+   * 用于定义本机输入选择的标志
+   * 在这种情况下，我们允许双 CMD + A 选择块
    *
    * @type {boolean}
    */
   private nativeInputSelected = false;
 
   /**
-   * Flag identifies any input selection
-   * That means we can select whole Block
+   * 标志标识任何输入选择
+   * 这意味着我们可以选择整个块
    *
    * @type {boolean}
    */
   private readyToBlockSelection = false;
 
   /**
-   * SelectionUtils instance
+   * 选择器工具实例
    *
    * @type {SelectionUtils}
    */
   private selection: SelectionUtils;
 
   /**
-   * Module Preparation
-   * Registers Shortcuts CMD+A and CMD+C
-   * to select all and copy them
+   * 模块的准备
+   * 注册快捷键 CMD+A 和 CMD+C
+   * 选择全部并复制
    */
   public prepare(): void {
     const { Shortcuts } = this.Editor;
@@ -150,7 +149,7 @@ export default class BlockSelection extends Module {
     this.selection = new SelectionUtils();
 
     /**
-     * CMD/CTRL+A selection shortcut
+     * CMD/CTRL+A 选择器快捷键
      */
     Shortcuts.add({
       name: 'CMD+A',
@@ -158,7 +157,7 @@ export default class BlockSelection extends Module {
         const { BlockManager, ReadOnly } = this.Editor;
 
         /**
-         * We use Editor's Block selection on CMD+A ShortCut instead of Browsers
+         * 我们在CMD + A ShortCut而不是浏览器上使用编辑器的块选择
          */
         if (ReadOnly.isEnabled) {
           event.preventDefault();
@@ -168,12 +167,12 @@ export default class BlockSelection extends Module {
         }
 
         /**
-         * When one page consist of two or more EditorJS instances
-         * Shortcut module tries to handle all events.
-         * Thats why Editor's selection works inside the target Editor, but
-         * for others error occurs because nothing to select.
+         * 当一页包含两个或多个EditorJS实例时
+         * 快捷键模块尝试处理所有事件。
+         * 这就是为什么编辑器的选择器在目标编辑器中起作用的原因，
+         * 但对于其他编辑器，则会发生错误，因为没有任何选择器。
          *
-         * Prevent such actions if focus is not inside the Editor
+         * 如果焦点不在编辑器内，则防止此类操作
          */
         if (!BlockManager.currentBlock) {
           return;
@@ -185,10 +184,10 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Toggle read-only state
+   * 只读状态切换
    *
-   *  - Remove all ranges
-   *  - Unselect all Blocks
+   *  - 删除所有范围选择器
+   *  - 取消所有 block 选择
    *
    * @param {boolean} readOnlyEnabled - "read only" state
    */
@@ -200,7 +199,7 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Remove selection of Block
+   * 删除块的选中
    *
    * @param {number?} index - Block index according to the BlockManager's indexes
    */
@@ -221,10 +220,10 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * Clear selection from Blocks
+   * 清除块中的选择
    *
-   * @param {Event} reason - event caused clear of selection
-   * @param {boolean} restoreSelection - if true, restore saved selection
+   * @param {Event} reason - 事件导致选择不明确
+   * @param {boolean} restoreSelection - 如果为 true，则恢复保存的选择
    */
   public clearSelection(reason?: Event, restoreSelection = false): void {
     const { BlockManager, Caret, RectangleSelection } = this.Editor;
@@ -237,8 +236,7 @@ export default class BlockSelection extends Module {
     const isPrintableKey = isKeyboard && _.isPrintableKey((reason as KeyboardEvent).keyCode);
 
     /**
-     * If reason caused clear of the selection was printable key and any block is selected,
-     * remove selected blocks and insert pressed key
+     * 如果导致无法选择的原因是可打印键，并且选择了任何块，请删除所选块并插入按下的键
      */
     if (this.anyBlockSelected && isKeyboard && isPrintableKey && !SelectionUtils.isSelectionExists) {
       const indexToInsert = BlockManager.removeSelectedBlocks();
@@ -249,8 +247,8 @@ export default class BlockSelection extends Module {
         const eventKey = (reason as KeyboardEvent).key;
 
         /**
-         * If event.key length >1 that means key is special (e.g. Enter or Dead or Unidentifier).
-         * So we use empty string
+         * 如果event.key长度> 1，则表示 key 是特殊的（例如 Enter，Dead 或 Unidentifier）。
+         * 所以我们使用空字符串
          *
          * @see https://developer.mozilla.org/ru/docs/Web/API/KeyboardEvent/key
          */
@@ -267,27 +265,26 @@ export default class BlockSelection extends Module {
     }
 
     /**
-     * Restore selection when Block is already selected
-     * but someone tries to write something.
+     * 当块已经被选中，但有人试图写入一些东西时，恢复选择。
      */
     if (restoreSelection) {
       this.selection.restore();
     }
 
-    /** Now all blocks cleared */
+    /** 现在清除所有块选中 */
     this.allBlocksSelected = false;
   }
 
   /**
-   * Reduce each Block and copy its content
+   * 减少每个块并复制其内容
    *
-   * @param {ClipboardEvent} e - copy/cut event
+   * @param {ClipboardEvent} e - 复制/剪切 事件
    *
    * @returns {Promise<void>}
    */
   public async copySelectedBlocks(e: ClipboardEvent): Promise<void> {
     /**
-     * Prevent default copy
+     * 防止默认复制
      */
     e.preventDefault();
 
@@ -295,7 +292,7 @@ export default class BlockSelection extends Module {
 
     this.selectedBlocks.forEach((block) => {
       /**
-       * Make <p> tag that holds clean HTML
+       * 生成 <p> 标记包含干净的 HTML
        */
       const cleanHTML = this.Editor.Sanitizer.clean(block.holder.innerHTML, this.sanitizerConfig);
       const fragment = $.make('p');
@@ -316,15 +313,15 @@ export default class BlockSelection extends Module {
   }
 
   /**
-   * select Block
+   * 选中块
    *
-   * @param {number?} index - Block index according to the BlockManager's indexes
+   * @param {number?} index - 块索引根据 BlockManager 的索引
    */
   public selectBlockByIndex(index?): void {
     const { BlockManager } = this.Editor;
 
     /**
-     * Remove previous focused Block's state
+     * 删除先前关注的块的状态
      */
     BlockManager.clearFocused();
 
@@ -336,7 +333,7 @@ export default class BlockSelection extends Module {
       block = BlockManager.getBlockByIndex(index);
     }
 
-    /** Save selection */
+    /** 保存选择 */
     this.selection.save();
     SelectionUtils.get()
       .removeAllRanges();
@@ -345,38 +342,37 @@ export default class BlockSelection extends Module {
 
     this.clearCache();
 
-    /** close InlineToolbar when we selected any Block */
+    /** 当我们选择任何块时，请关闭 InlineToolbar */
     this.Editor.InlineToolbar.close();
   }
 
   /**
-   * Clear anyBlockSelected cache
+   * 清除 anyBlockSelected 缓存
    */
   public clearCache(): void {
     this.anyBlockSelectedCache = null;
   }
 
   /**
-   * Module destruction
-   * De-registers Shortcut CMD+A
+   * 模块破坏
+   * 注销快捷键 CMD + A
    */
   public destroy(): void {
     const { Shortcuts } = this.Editor;
 
-    /** Selection shortcut */
+    /** 选择器快键键 */
     Shortcuts.remove('CMD+A');
   }
 
   /**
-   * First CMD+A selects all input content by native behaviour,
-   * next CMD+A keypress selects all blocks
+   * 首先 CMD+A 通过原生行为选择所有输入内容，然后 CMD+A 按键选择所有块
    *
-   * @param {KeyboardEvent} event - keyboard event
+   * @param {KeyboardEvent} event - 键盘事件
    */
   private handleCommandA(event: KeyboardEvent): void {
     this.Editor.RectangleSelection.clearSelection();
 
-    /** allow default selection on native inputs */
+    /** 允许对原生输入框进行默认选择 */
     if ($.isNativeInput(event.target) && !this.readyToBlockSelection) {
       this.readyToBlockSelection = true;
 
@@ -387,8 +383,8 @@ export default class BlockSelection extends Module {
     const inputs = workingBlock.inputs;
 
     /**
-     * If Block has more than one editable element allow native selection
-     * Second cmd+a will select whole Block
+     * 如果“块”具有多个可编辑元素，则允许进行本机选择
+     * 第二个 CMD+A 将选择整个块
      */
     if (inputs.length > 1 && !this.readyToBlockSelection) {
       this.readyToBlockSelection = true;
@@ -404,60 +400,60 @@ export default class BlockSelection extends Module {
 
     if (this.needToSelectAll) {
       /**
-       * Prevent default selection
+       * 防止默认选择器
        */
       event.preventDefault();
 
       this.selectAllBlocks();
 
       /**
-       * Disable any selection after all Blocks selected
+       * 选中所有块后禁用任何选择
        */
       this.needToSelectAll = false;
       this.readyToBlockSelection = false;
 
       /**
-       * Close ConversionToolbar when all Blocks selected
+       * 选中所有块后，关闭ConversionToolbar
        */
       this.Editor.ConversionToolbar.close();
     } else if (this.readyToBlockSelection) {
       /**
-       * prevent default selection when we use custom selection
+       * 当我们使用自定义选择器时，阻止默认选择器
        */
       event.preventDefault();
 
       /**
-       * select working Block
+       * 选中工作中的 block
        */
       this.selectBlockByIndex();
 
       /**
-       * Enable all Blocks selection if current Block is selected
+       * 如果选择了当前块，则启用所有块选择
        */
       this.needToSelectAll = true;
     }
   }
 
   /**
-   * Select All Blocks
-   * Each Block has selected setter that makes Block copyable
+   * 选中所有 block
+   * 每个块都有选定的setter，可以使块可复制
    */
   private selectAllBlocks(): void {
     /**
-     * Save selection
-     * Will be restored when closeSelection fired
+     * 保存所有选中块
+     * 当 closeSelection 被触发时将被恢复
      */
     this.selection.save();
 
     /**
-     * Remove Ranges from Selection
+     * 从选择器中删除范围选区
      */
     SelectionUtils.get()
       .removeAllRanges();
 
     this.allBlocksSelected = true;
 
-    /** close InlineToolbar if we selected all Blocks */
+    /** 如果我们选择了所有块，则关闭 InlineToolbar */
     this.Editor.InlineToolbar.close();
   }
 }
